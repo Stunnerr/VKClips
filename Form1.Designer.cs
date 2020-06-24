b571f61aad0b870198125f42f308708489be7c4c
Stunner <semgol824@gmail.com>
2020-06-24 18:48:17 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace VKClips
+{
+    partial class Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.token = new System.Windows.Forms.TextBox();
+            this.Execute = new System.Windows.Forms.Button();
+            this.OpenMP4 = new System.Windows.Forms.OpenFileDialog();
+            this.FPath = new System.Windows.Forms.TextBox();
+            this.PathL = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.TextBox();
+            this.TitleL = new System.Windows.Forms.Label();
+            this.DescL = new System.Windows.Forms.Label();
+            this.Description = new System.Windows.Forms.TextBox();
+            this.GroupL = new System.Windows.Forms.Label();
+            this.GID = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(527, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VKClips";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Access token:";
+            // 
+            // token
+            // 
+            this.token.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.token.HideSelection = false;
+            this.token.Location = new System.Drawing.Point(121, 90);
+            this.token.MaxLength = 100;
+            this.token.Name = "token";
+            this.token.Size = new System.Drawing.Size(418, 23);
+            this.token.TabIndex = 2;
+            this.token.UseSystemPasswordChar = true;
+            this.token.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tokenInput);
+            this.token.Leave += new System.EventHandler(this.CheckToken);
+            // 
+            // Execute
+            // 
+            this.Execute.Enabled = false;
+            this.Execute.Location = new System.Drawing.Point(12, 257);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(527, 64);
+            this.Execute.TabIndex = 3;
+            this.Execute.Text = "ВЗЛОМ";
+            this.Execute.UseVisualStyleBackColor = true;
+            this.Execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // OpenMP4
+            // 
+            this.OpenMP4.Filter = "Видео (*.mp4)|*.mp4";
+            this.OpenMP4.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // FPath
+            // 
+            this.FPath.Location = new System.Drawing.Point(121, 120);
+            this.FPath.Name = "FPath";
+            this.FPath.Size = new System.Drawing.Size(393, 23);
+            this.FPath.TabIndex = 4;
+            // 
+            // PathL
+            // 
+            this.PathL.AutoSize = true;
+            this.PathL.Location = new System.Drawing.Point(38, 123);
+            this.PathL.Name = "PathL";
+            this.PathL.Size = new System.Drawing.Size(80, 15);
+            this.PathL.TabIndex = 1;
+            this.PathL.Text = "Путь к видео:";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(513, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "...";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Title
+            // 
+            this.Title.Location = new System.Drawing.Point(121, 149);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(418, 23);
+            this.Title.TabIndex = 6;
+            // 
+            // TitleL
+            // 
+            this.TitleL.AutoSize = true;
+            this.TitleL.Location = new System.Drawing.Point(20, 152);
+            this.TitleL.Name = "TitleL";
+            this.TitleL.Size = new System.Drawing.Size(98, 15);
+            this.TitleL.TabIndex = 7;
+            this.TitleL.Text = "Название клипа:";
+            // 
+            // DescL
+            // 
+            this.DescL.AutoSize = true;
+            this.DescL.Location = new System.Drawing.Point(17, 181);
+            this.DescL.Name = "DescL";
+            this.DescL.Size = new System.Drawing.Size(101, 15);
+            this.DescL.TabIndex = 7;
+            this.DescL.Text = "Описание клипа:";
+            // 
+            // Description
+            // 
+            this.Description.Location = new System.Drawing.Point(121, 178);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(418, 23);
+            this.Description.TabIndex = 6;
+            // 
+            // GroupL
+            // 
+            this.GroupL.AutoSize = true;
+            this.GroupL.Location = new System.Drawing.Point(53, 210);
+            this.GroupL.Name = "GroupL";
+            this.GroupL.Size = new System.Drawing.Size(65, 15);
+            this.GroupL.TabIndex = 7;
+            this.GroupL.Text = "ID группы:";
+            // 
+            // GID
+            // 
+            this.GID.Enabled = false;
+            this.GID.Location = new System.Drawing.Point(121, 207);
+            this.GID.Name = "GID";
+            this.GID.Size = new System.Drawing.Size(393, 23);
+            this.GID.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(520, 212);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.GIDCheck);
+            // 
+            // Form
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 333);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.GID);
+            this.Controls.Add(this.GroupL);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.DescL);
+            this.Controls.Add(this.TitleL);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PathL);
+            this.Controls.Add(this.FPath);
+            this.Controls.Add(this.Execute);
+            this.Controls.Add(this.token);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form";
+            this.Text = "VKClips v1.0";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox token;
+        private System.Windows.Forms.Button Execute;
+        private System.Windows.Forms.OpenFileDialog OpenMP4;
+        private System.Windows.Forms.TextBox FPath;
+        private System.Windows.Forms.Label PathL;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Title;
+        private System.Windows.Forms.Label TitleL;
+        private System.Windows.Forms.Label DescL;
+        private System.Windows.Forms.TextBox Description;
+        private System.Windows.Forms.Label GroupL;
+        private System.Windows.Forms.TextBox GID;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace VKClips
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        string access_token = "";
+        public Form()
+        {
+            InitializeComponent();
+        }
+        private string UploadApi(Dictionary<string, string> param = null)
+        {
+            param ??= new Dictionary<string, string>();
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36");
+            webClient.Headers.Add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            webClient.QueryString.Add("v", "5.107");
+            webClient.QueryString.Add("access_token", access_token);
+            foreach (var i in param)
+            {
+                webClient.QueryString.Add(i.Key, i.Value);
+            }
+            return webClient.DownloadString("https://api.vk.com/method/shortVideo.create");
+
+        }
+        private void UploadClip(string file, Dictionary<string, string> param)
+        {
+            string link = UploadApi(param);
+            JObject jObject = JObject.Parse(link);
+            string uploadURL = jObject.SelectToken("$.response.upload_url").Value<string>();
+            WebClient webClient = new WebClient();
+            webClient.UploadFile(uploadURL, file);
+
+        }
+        private void TestApi()
+        {
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36");
+            webClient.Headers.Add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            webClient.QueryString.Add("v", "5.107");
+            webClient.QueryString.Add("access_token", access_token);
+            ChangeColor(webClient.DownloadString("https://api.vk.com/method/execute").Contains("\"error_code\":5"));
+
+        }
+        private void tokenInput(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetterOrDigit(e.KeyChar) && !char.IsControl(e.KeyChar)) e.Handled = true;
+        }
+        private async void ChangeColor(bool test)
+        {
+            if (test)
+            {
+                token.BackColor = Color.Red;
+                Execute.Enabled = false;
+            }
+            else { token.BackColor = Color.White; Execute.Enabled = true; }
+        }
+        private void CheckToken(object sender, EventArgs e)
+        {
+            if (token.TextLength < 60)
+            {
+                ChangeColor(true);
+            }
+            else
+            {
+                access_token = token.Text;
+                TestApi();
+            }
+        }
+        string realfile = "";
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            FPath.Text = OpenMP4.FileName;
+            realfile = OpenMP4.FileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenMP4.ShowDialog();
+        }
+
+        private void GIDCheck(object sender, EventArgs e)
+        {
+            GID.Enabled = checkBox1.Checked;
+        }
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            if (File.Exists(FPath.Text))
+            {
+                string file = FPath.Text;
+                if (Title.Text.Length > 1)
+                {
+                    param.Add("title", Title.Text);
+                }
+                if (Description.Text.Length > 1)
+                {
+                    param.Add("description", Description.Text);
+                }
+                if (checkBox1.Checked && GID.Text.Length > 2)
+                {
+                    param.Add("group_id", GID.Text);
+                }
+                param.Add("file_size", new FileInfo(file).Length.ToString());
+                UploadClip(file, param);
+            }
+        }
+
+        private void Form_DragDrop(object sender, DragEventArgs e)
+        {
+            string s = ((string[])e.Data.GetData(DataFormats.FileDrop, false))[0];
+            if (new FileInfo(s).Exists) if (new FileInfo(s).Extension == ".mp4") FPath.Text = s;
+        }
+
+        private void Form_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                if (new FileInfo(((string[])e.Data.GetData(DataFormats.FileDrop, false))[0]).Extension == ".mp4")
+                    e.Effect = DragDropEffects.All;
+                else e.Effect = DragDropEffects.None;
+            }
+            else
+                e.Effect = DragDropEffects.None;
+        }
+    }
+}
